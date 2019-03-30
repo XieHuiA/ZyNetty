dee35582b7617677fb61f0fcbeb38778d0a1ed4e
mine <requestxie@126.com>
2019-03-30 12:22:54 +0800

    first commit

---
+++ src/main/java/com/zy/netty/server/ServerBootstrap.java
@@ -0,0 +1,22 @@
+package com.zy.netty.server;
+
+/**
+ - - - - - - - - - - - - - - - - - - - - -
+ Author    :  [Xie hui]
+ Date      :  2019-03-29 20:53
+ explain   :
+ - - - - - - - - - - - - - - - - - - - - -
+ **/
+
+public class ServerBootstrap {
+
+    public static void main(String[] args) {
+
+        System.out.println("start the zyServer");
+
+    }
+
+
+
+
+}
